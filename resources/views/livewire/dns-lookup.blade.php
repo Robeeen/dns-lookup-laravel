82136010a21edfee1cf4bc4589d5ad685094d152
Robeeen <robeeen@gmail.com>
2024-05-24 09:51:25 +0600

    updated

--- resources/views/livewire/dns-lookup.blade.php
+++ resources/views/livewire/dns-lookup.blade.php
@@ -1,12 +1,28 @@
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <div>
 
-    <form wire:submit="createNewuser" action="">
-        <input wire:model="name" type="text" placeholder="name" />
-        <input wire:model="email" type="email" placeholder="email" />
-        <input wire:model="password" type="password" placeholder="Password" />
+    <form wire:submit="createNewuser" action="" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+       
+            <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                <input wire:model="name" type="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="your name" required />
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input wire:model="email" type="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+            </div>
+            <div class="mb-5">
+                <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your gender</label>
+                <input wire:model="gender" type="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="your gender" required />
+            <div>
+            <div class="mb-5">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input wire:model="password" type="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="strong password" required />
+            <div>
 
-        <button>Create</button>
+        <button style="padding: 5px; border: 1px solid; font-weight:bold;margin-top: 10px; background: blue; color: white; width:100%">Create</button>
     </form>
 
     <hr>
